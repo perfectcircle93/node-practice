19e2356ad13a4fd7dc508aa7d6c467b3603df9ef
Magdalena Świderska <m.swiderska@outlook.com>
2020-09-26 22:41:25 +0200

    Add people.json

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
-const os = require('os');
+const fs = require('fs');
+
+const {gender, maleFirstNames, femaleFirstNames, lastNames} = require('./store');
+
+const people = [];
+
+const randChoice = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const randNumber = (min, max) => {
+    return Math.floor((Math.random() * (max - min)) + min)
+}
+
+for ( let i = 0; i < 21; i++) {
+
+    let randGender = (randChoice(gender));
+
+    if (randGender == 'male') {
+        randFirstName = (randChoice(maleFirstNames));
+    } else {
+        randFirstName = (randChoice(femaleFirstNames));
+    };
+
+    let randLastName = (Math.random(lastNames));
+
+    let randAge = (randNumber(18, 78));
+
+    people.push({
+        gender : randGender,
+        firstName : randFirstName,
+        lastName : randLastName,
+        age : randAge
+    });
+}
+
+const peopleJsonString = JSON.stringify(people, null, ' ');
 
 
-console.log('Platform: ', os.platform());
-console.log('Arch: ', os.arch());
-console.log('UserInfo', os.userInfo());
+
+
+fs.writeFile('people.json', peopleJsonString, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+
+
---
+++ store.js
@@ -0,0 +1,42 @@
+const gender = [
+    'male',
+    'female'
+  ]
+  const maleFirstNames = [
+    'Marek',
+    'Nataniel',
+    'Mateusz',
+    'Paweł',
+    'Łukasz',
+    'Michał',
+    'Tomek',
+    'Janek'
+  ];
+  
+  const femaleFirstNames = [
+    'Anna',
+    'Agnieszka',
+    'Alicja',
+    'Gosia',
+    'Kinga',
+    'Valeria',
+    'Jessica',
+    'Laura'
+  ];
+  
+  const lastNames = [
+    'Barszcz',
+    'Nowak',
+    'Lipiec',
+    'Bąk',
+    'Antol',
+    'Kot',
+    'Busler',
+    'Szerman',
+    'Monte'
+  ];
+  
+  exports.gender = gender;
+  exports.maleFirstNames = maleFirstNames;
+  exports.femaleFirstNames = femaleFirstNames;
+  exports.lastNames = lastNames;
